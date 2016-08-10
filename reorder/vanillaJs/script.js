793afa36c26b1bb220744421a84edbcc9f5bb1aa
Jonathan Lam <jlam55555@gmail.com>
2016-08-10 17:09:10 -0400

    Added preliminary vanilla-js reorder

---
+++ reorder/vanillaJs/script.js
@@ -0,0 +1,48 @@
+var Reorderable = function(container) {
+  var reorderable = container.children;
+  var reorderableArr = Array.from(reorderable);
+  var todrag; 
+  var getNearest = function() {
+    return Math.round((event.pageY - container.offsetTop) / container.clientHeight * reorderable.length);
+  };
+  var getYCoor = function(y) {
+    return y < container.offsetTop ? 0 : y > container.offsetTop + container.clientHeight ? container.clientHeight : y - container.offsetTop;
+  };
+  document.body.addEventListener("mousemove", function(event) {
+    if(!todrag) return;
+    todrag.style.top = getYCoor(event.pageY)-10 + "px";
+    console.log(todrag.style.top);
+    var nearest = getNearest(); 
+    for(elem of reorderableArr)
+      elem.style.marginTop = "0";
+    if(nearest >= 0 && nearest < reorderable.length)
+      reorderable.item(nearest).style.marginTop = todrag.clientHeight + 10 + "px";
+  });
+  for(elem of reorderableArr) {
+    elem.addEventListener("click", function() {
+      if(todrag && todrag != this) return;
+      if(!todrag) {
+        this.style.position = "absolute";
+        todrag = this;
+        document.dispatchEvent(new Event("mousemove"));
+      } else {
+        this.style.position = "static";
+        todrag = null;
+        var nearest = getNearest();
+        console.log(reorderable.item(nearest).textContent, reorderable.item(nearest).style.marginTop);
+        if(nearest != reorderableArr.indexOf(this))
+          container.insertBefore(this, reorderable.item(nearest));
+        for(elem of reorderableArr)
+          elem.style.marginTop = "0";
+      }
+    });
+  }
+  document.addEventListener("click", function() {
+    if(todrag) {
+      //todrag.dispatchEvent(new Event("click"));
+    }
+  });
+};
+window.onload = function() {
+  new Reorderable(document.getElementById("list"));
+};
